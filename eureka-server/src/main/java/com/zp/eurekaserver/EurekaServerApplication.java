3b1c799bfd4e0733436a96ecf277c8e03101f46d
sdzp <zpzp6@qq.com>
2019-09-15 20:01:58 +0800

    Springcloud项目上传到GigHub上

---
+++ eureka-server/src/main/java/com/zp/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.zp.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心
+ * 1、配置eureka信息
+ * 2、启用eureka功能 @EnableEurekaServer
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
